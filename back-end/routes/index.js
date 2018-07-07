e92d961725fac4004667c1a1a5ce5f6bcb30c2c1
gananggww <gananggww@gmail.com>
2018-07-07 21:12:46 +0700

    update

--- back-end/routes/index.js
+++ back-end/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 const data_nilai = require('../controller/data_nilai.js');
 
 /* GET home page. */
-router.post('/', data_nilai.get_data_nilai)
+router.get('/', data_nilai.get_data_nilai)
 
 
 module.exports = router;
